1164304975681de7c7043ca97585b1aa471e9a6b
GurmanVIT <gurmandeep.vit@gmail.com>
2023-12-13 14:34:40 +0530

    new code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+
+
 
 function App() {
+
+  const [users, setUser] = useState([])
+  useEffect(() => {
+    fetch("https://reqres.in/api/users?page=2").then((result) => {
+      result.json().then((resp) => {
+        console.log('ds ', resp)
+        setUser(resp.data)
+      })
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Get API Call </h1>
+      <table border="1">
+        <tbody>
+          <tr>
+            <td>ID</td>
+            <td>Email</td>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Avatar</td>
+          </tr>
+          {
+            users.map((item, i) =>
+              <tr key={i}>
+                <td>{item.id}</td>
+                <td>{item.email}</td>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+                <td>
+                  <img src={item.avatar} alt={'${item.first_name} Avatar'} style={{ width: '50px', height: '50px' }} />
+
+                </td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
-
 export default App;
